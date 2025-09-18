49952716818bb78b19e0512bbcab0ba093905b4b
UnicornOnAzur <unicorn.on.azur@gmail.com>
2025-09-18 17:05:30 +0200

    Change imports

---
+++ src/geo_toolbox/__init__.py
@@ -0,0 +1,4 @@
+from distances import (DistanceUnit,
+                       calculate_haversine_distance,
+                       calculate_law_of_cosines_distance,
+                       calculate_vincenty_distance)
