b85f011decec72406dcc00db87f07f7f5b5bda54
UnicornOnAzur <unicorn.on.azur@gmail.com>
2025-09-19 22:16:31 +0200

    redo wrapper :

--- src/geo_toolbox/1__init__.py
+++ src/geo_toolbox/1__init__.py
@@ -1,4 +1,4 @@
-from .distances import (DistanceUnit,
+from .distances import (DistanceUnit,  # noqa: F401
                         calculate_haversine_distance,
                         calculate_law_of_cosines_distance,
                         calculate_vincenty_distance)
@@ -7,4 +7,8 @@ from .wrappers.wrapper_cartopy import (equal_earth_cartopy)
 __all__ = ["DistanceUnit",
            "calculate_haversine_distance",
            "calculate_vincenty_distance",
-           "equal_earth_cartopy"]
+           "equal_earth_cartopy"
+           ]
+
+if __name__ == "__main__":
+    pass
