f910aab4393ce00ecfbebb91bd25193ebde04a90
UnicornOnAzur <unicorn.on.azur@gmail.com>
2025-09-19 17:01:27 +0200

    Add __all__

--- src/geo_toolbox/__init__.py
+++ src/geo_toolbox/__init__.py
@@ -4,8 +4,7 @@ from .distances import (DistanceUnit,
                         calculate_vincenty_distance)
 from .wrappers.wrapper_cartopy import (equal_earth_cartopy)
 
-__all__ = [DistanceUnit,
-           calculate_haversine_distance,
-#            calculate_law_of_cosines_distance,
-           calculate_vincenty_distance,
-           equal_earth_cartopy]
+__all__ = ["DistanceUnit",
+           "calculate_haversine_distance",
+           "calculate_vincenty_distance",
+           "equal_earth_cartopy"]
