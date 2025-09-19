e45453ea5efc5c369b9faa2bd2889132ecade520
UnicornOnAzur <unicorn.on.azur@gmail.com>
2025-09-19 08:41:26 +0200

    Use relative improt

--- src/geo_toolbox/__init__.py
+++ src/geo_toolbox/__init__.py
@@ -1,4 +1,4 @@
-from distances import (DistanceUnit,
+from .distances import (DistanceUnit,
                        calculate_haversine_distance,
                        calculate_law_of_cosines_distance,
                        calculate_vincenty_distance)
